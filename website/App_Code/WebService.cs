2dfa0ff5e085cf5b03667fd7341b68aa58d8ea0f
The Florin <coadaflorin@gmail.com>
2021-04-29 15:16:16 +0100

    test

--- website/App_Code/WebService.cs
+++ website/App_Code/WebService.cs
@@ -70,7 +70,7 @@ namespace Altoro
       myConnection.ConnectionString = ConfigurationManager.ConnectionStrings["DBConnStr"].ConnectionString;
       myConnection.Open();
 
-      string query = "SELECT * From users WHERE userid = " + UserId1;
+      string query = "SELECT * From users WHERE userid = " + UserId;
       OleDbDataAdapter myAccounts = new OleDbDataAdapter(query, myConnection);
 
       DataSet ds = new DataSet();
